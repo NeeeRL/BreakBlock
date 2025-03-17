9bb7d5a57f68e4ca92a370dcb23b0dfa2f906701
NeeeRL <kenken.s5049@gmail.com>
2025-03-17 12:17:26 +0900

    playerの制御をできるようにして、retryボタンを右下に完備した。

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -3,21 +3,25 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float speed = 1.0f;
+    public GameManager gameManager;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow)) 
+        if(gameManager.isGameNow)
         {
-            if(this.transform.position.x > -4)
+            if(Input.GetKey(KeyCode.LeftArrow)) 
             {
-                this.transform.position += Vector3.left  * speed * Time.deltaTime;
+                if(this.transform.position.x > -4)
+                {
+                    this.transform.position += Vector3.left  * speed * Time.deltaTime;
+                }
             }
-        }
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            if(this.transform.position.x < 4)
-                this.transform.position += Vector3.right * speed * Time.deltaTime;
+            if(Input.GetKey(KeyCode.RightArrow))
+            {
+                if(this.transform.position.x < 4)
+                    this.transform.position += Vector3.right * speed * Time.deltaTime;
+            }
         }
     }
 }
